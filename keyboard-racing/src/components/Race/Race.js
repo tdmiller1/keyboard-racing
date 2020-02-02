44597b1265050167ca75eb44b49c404d6bebe878
Ben Kahlert <benjaminjosefkahlert@gmail.com>
2020-02-01 20:27:12 -0500

    Add typing prompt classes

--- keyboard-racing/src/components/Race/Race.js
+++ keyboard-racing/src/components/Race/Race.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import TypingPrompt from '../TypingPrompt/TypingPrompt';
+import './Race.scss';
 
 class Race extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            value: ''
+            correctText: 'Thi',
+            wrongText: 's i',
+            todoText: 's a typing prompt',
+            inputValue: ''
         };
     }
 
     handleChange = (e) => {
-        this.setState({ value: e.target.value });
+        this.setState({ inputValue: e.target.value });
     }
 
     render = () => {
         return (
             <div>
-                <TypingPrompt />
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
+                <div className="typing-prompt-container">
+                    <span className="typing-prompt-correct">{this.state.correctText}</span>
+                    <span className="typing-prompt-wrong">{this.state.wrongText}</span>
+                    <span className="typing-prompt-todo">{this.state.todoText}</span>
+                </div>
+                <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
             </div>
         );
     }
