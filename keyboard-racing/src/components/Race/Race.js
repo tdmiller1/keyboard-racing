a6f3734875984b81b6527a3d7baf258306bf05a7
Ben Kahlert <benjaminjosefkahlert@gmail.com>
2020-02-01 21:04:17 -0500

    Add Race component logic

--- keyboard-racing/src/components/Race/Race.js
+++ keyboard-racing/src/components/Race/Race.js
@@ -6,15 +6,55 @@ class Race extends React.Component {
     constructor() {
         super();
         this.state = {
-            correctText: 'Thi',
-            wrongText: 's i',
-            todoText: 's a typing prompt',
+            correctText: '',
+            wrongText: '',
+            todoText: 'This is a typing prompt!',
             inputValue: ''
         };
     }
 
-    handleChange = (e) => {
-        this.setState({ inputValue: e.target.value });
+    type = (e) => {
+        const newString = e.target.value;
+        const newestChar = newString.charAt(newString.length - 1);
+        const wasBackspacePressed = newString.length <= this.state.inputValue.length;
+
+        if (this.state.todoText) {
+            if (wasBackspacePressed)
+                this.backspace()
+            else if (newestChar === this.state.todoText.charAt(0))
+                this.correctInput(newestChar)
+            else
+                this.incorrectInput(newestChar)
+
+            this.setState({ inputValue: newString });
+        }
+    }
+
+    backspace = () => {
+        let wrongText = this.state.wrongText, correctText = this.state.correctText;
+
+        if (wrongText.length)
+            wrongText = wrongText.substr(0, wrongText.length - 1);
+        else if (correctText.length)
+            correctText = correctText.substr(0, correctText.length - 1)
+
+        this.setState({
+            correctText,
+            wrongText
+        });
+    }
+
+    correctInput = (newestChar) => {
+        this.setState({
+            correctText: this.state.correctText + newestChar,
+            todoText: this.state.todoText.substr(1, this.state.todoText.length)
+        });
+    }
+
+    incorrectInput = (newestChar) => {
+        this.setState({
+            wrongText: this.state.wrongText + newestChar
+        })
     }
 
     render = () => {
@@ -25,7 +65,7 @@ class Race extends React.Component {
                     <span className="typing-prompt-wrong">{this.state.wrongText}</span>
                     <span className="typing-prompt-todo">{this.state.todoText}</span>
                 </div>
-                <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
+                <input type="text" value={this.state.inputValue} onChange={this.type} />
             </div>
         );
     }
