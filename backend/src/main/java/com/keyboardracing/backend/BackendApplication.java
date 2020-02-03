29bbb88a3de5d5e3a76a58fc29964f7a1332a22b
tdmiller1 <tdmiller7@bsu.edu>
2020-02-03 18:58:03 -0500

    Inital commit for backend service

--- backend/src/main/java/com/keyboardracing/backend/BackendApplication.java
+++ backend/src/main/java/com/keyboardracing/backend/BackendApplication.java
@@ -1,13 +1,13 @@
 package com.keyboardracing.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
